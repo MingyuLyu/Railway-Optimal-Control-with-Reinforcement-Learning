a7a289a554e2443bdd665e8ffeaf47ae4786ac87
MingyuLyu <148462787+MingyuLyu@users.noreply.github.com>
2024-10-18 13:03:37 +0900

    test with old environment

--- SAC-Continuous-Pytorch-main/main.py
+++ SAC-Continuous-Pytorch-main/main.py
@@ -5,11 +5,11 @@ import gymnasium as gym
 import os, shutil
 import argparse
 import torch
-
+from ESARS import TrainSpeedControl
 
 '''Hyperparameter Setting'''
 parser = argparse.ArgumentParser()
-parser.add_argument('--dvc', type=str, default='cuda', help='running device: cuda or cpu')
+parser.add_argument('--dvc', type=str, default='cpu', help='running device: cuda or cpu')
 parser.add_argument('--EnvIdex', type=int, default=0, help='PV1, Lch_Cv2, Humanv4, HCv4, BWv3, BWHv3')
 parser.add_argument('--write', type=str2bool, default=False, help='Use SummaryWriter to record the training')
 parser.add_argument('--render', type=str2bool, default=False, help='Render or Not')
@@ -39,13 +39,15 @@ def main():
     BrifEnvName = ['PV1', 'LLdV2', 'Humanv4', 'HCv4','BWv3', 'BWHv3']
 
     # Build Env
-    env = gym.make(EnvName[opt.EnvIdex], render_mode = "human" if opt.render else None)
-    eval_env = gym.make(EnvName[opt.EnvIdex])
+    # env = gym.make(EnvName[opt.EnvIdex], render_mode="human" if opt.render else None)
+    # eval_env = gym.make(EnvName[opt.EnvIdex])
+    env = TrainSpeedControl()
+    eval_env = TrainSpeedControl()
     opt.state_dim = env.observation_space.shape[0]
     opt.action_dim = env.action_space.shape[0]
     opt.max_action = float(env.action_space.high[0])   #remark: action space【-max,max】
     opt.max_e_steps = env._max_episode_steps
-    print(f'Env:{EnvName[opt.EnvIdex]}  state_dim:{opt.state_dim}  action_dim:{opt.action_dim}  '
+    print(f'Env:TrainSpeedControl  state_dim:{opt.state_dim}  action_dim:{opt.action_dim}  '
           f'max_a:{opt.max_action}  min_a:{env.action_space.low[0]}  max_e_steps:{opt.max_e_steps}')
 
     # Seed Everything
@@ -61,7 +63,7 @@ def main():
         from torch.utils.tensorboard import SummaryWriter
         timenow = str(datetime.now())[0:-10]
         timenow = ' ' + timenow[0:13] + '_' + timenow[-2::]
-        writepath = 'runs/{}'.format(BrifEnvName[opt.EnvIdex]) + timenow
+        writepath = 'runs/{}'.format('TrainSpeedControl') + timenow
         if os.path.exists(writepath): shutil.rmtree(writepath)
         writer = SummaryWriter(log_dir=writepath)
 
@@ -69,12 +71,12 @@ def main():
     # Build DRL model
     if not os.path.exists('model'): os.mkdir('model')
     agent = SAC_countinuous(**vars(opt)) # var: transfer argparse to dictionary
-    if opt.Loadmodel: agent.load(BrifEnvName[opt.EnvIdex], opt.ModelIdex)
+    if opt.Loadmodel: agent.load('TrainSpeedControl', opt.ModelIdex)
 
     if opt.render:
         while True:
             score = evaluate_policy(env, agent, turns=1)
-            print('EnvName:', BrifEnvName[opt.EnvIdex], 'score:', score)
+            print('EnvName:', 'TrainSpeedControl', 'score:', score)
     else:
         total_steps = 0
         while total_steps < opt.Max_train_steps:
@@ -91,7 +93,7 @@ def main():
                     a = agent.select_action(s, deterministic=False)  # a∈[-1,1]
                     act = Action_adapter(a, opt.max_action)  # act∈[-max,max]
                 s_next, r, dw, tr, info = env.step(act)  # dw: dead&win; tr: truncated
-                r = Reward_adapter(r, opt.EnvIdex)
+                # r = Reward_adapter(r, opt.EnvIdex)
                 done = (dw or tr)
 
                 agent.replay_buffer.add(s, a, r, s_next, dw)
@@ -108,11 +110,11 @@ def main():
                 if total_steps % opt.eval_interval == 0:
                     ep_r = evaluate_policy(eval_env, agent, turns=3)
                     if opt.write: writer.add_scalar('ep_r', ep_r, global_step=total_steps)
-                    print(f'EnvName:{BrifEnvName[opt.EnvIdex]}, Steps: {int(total_steps/1000)}k, Episode Reward:{ep_r}')
+                    print(f'EnvName:TrainSpeedControl, Steps: {int(total_steps/1000)}k, Episode Reward:{ep_r}')
 
                 '''save model'''
                 if total_steps % opt.save_interval == 0:
-                    agent.save(BrifEnvName[opt.EnvIdex], int(total_steps/1000))
+                    agent.save('TrainSpeedControl', int(total_steps/1000))
         env.close()
         eval_env.close()
 
